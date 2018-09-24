ac93f2763e48b9495527504bd1a4f3163db446c3
xtuxur <alex.delzar@gmail.com>
2018-09-24 09:59:19 -0500

    empezamos

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CreditoWeb.Models;
+
+namespace CreditoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Validar(string tarjetanum)
+        {
+            /// Creamos una tarjeta 
+            Tarjeta tarjeta = new Tarjeta(tarjetanum);
+            return Ok(tarjeta);
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Tarjeta.cs
@@ -0,0 +1,54 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace CreditoWeb.Models
+{
+    public class Tarjeta
+    {
+        [Required(ErrorMessage = "El número de tarjeta es necesario.")]
+        [CreditCard]
+        public string TarjetaNum { get; set; }
+        public TipoTarjeta TipoTarjeta { get; set; }
+
+        public bool Valida { get; set; }
+
+        public Tarjeta(string tarjetaNum)
+        {
+            this.TarjetaNum = tarjetaNum;
+            Valida = esValida();
+            TipoTarjeta = tipoDeTarjeta();
+        }
+
+
+        /// Basado en el algoritmo de Luhn determinar si esta tarjeta es valida
+        /// como estamos dentro de la clase de tarjeta tenemos acceso a la propiedad TarjetaNum 
+        private bool esValida()
+        {
+            return false;
+
+        }
+
+
+        /// Si la tarjeta es valida determinar de cuál tipo es VISA, MASTERCARD, AMERICANEXPRESS
+        /// como estamos dentro de la clase de tarjeta tenemos acceso a la propiedad TarjetaNum 
+        private TipoTarjeta tipoDeTarjeta()
+        {
+            
+            return TipoTarjeta.NOVALIDA;
+        }
+
+
+
+    }
+
+    public enum TipoTarjeta
+    {
+        VISA,
+        MASTERCARD,
+        AMERICANEXPRESS,
+        NOVALIDA
+
+
+    }
+}
